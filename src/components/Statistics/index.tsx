a6267dcd6e7f2e9dd0f263930e88101dfac8adf1
alexandru.simion <alexandru.simion@student.upt.ro>
2024-06-08 17:03:40 +0300

    added labels

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -3,24 +3,20 @@ import "./Statistics.css";
 import { CategorySpending } from "../../API/Models/CategorySpending";
 import { CategoriesApiClient } from "../../API/Clients/CategoriesApiClient";
 import { Box, Divider } from "@mui/material";
-import { Label } from "@mui/icons-material";
 import { Pie } from "react-chartjs-2";
 import "chart.js/auto";
 
 export const Statistics: FC = () => {
-  const [categorySpendings, setCategorySpendings] = useState<
-    CategorySpending[]
-  >([]);
+  const [categorySpendings, setCategorySpendings] = useState<CategorySpending[]>([]);
 
-
-  const processData = (data:CategorySpending[]) =>{
-    return{
-      Labels:data.map((item) => item.name),
-      datasets:[
+  const processData = (data: CategorySpending[]) => {
+    return {
+      labels: data.map((item) => item.name),
+      datasets: [
         {
-          Lable:"Spending",
-          data:data.map((item) => item.totalSpent),
-          backgroundColor:[
+          label: "Spending",
+          data: data.map((item) => item.totalSpent),
+          backgroundColor: [
             "rgba(255, 99, 132, 0.6)",
             "rgba(54, 162, 235, 0.6)",
             "rgba(255, 206, 86, 0.6)",
@@ -29,45 +25,42 @@ export const Statistics: FC = () => {
             "rgba(255, 159, 64, 0.6)",
             "rgba(155, 159, 64, 0.6)",
           ],
-        }
-      ]
-    }
-  }
+        },
+      ],
+    };
+  };
 
-  const fetchSpendings = async() => {
-    try{
+  const fetchSpendings = async () => {
+    try {
       const res = await CategoriesApiClient.getSpendingAsync();
       const spendings = res.categories.map(
-        (e:CategorySpending) => ({ ...e } as CategorySpending)
+        (e: CategorySpending) => ({ ...e } as CategorySpending)
       );
       setCategorySpendings(spendings);
-    }
-    catch(error:any)
-    {
+    } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() =>
-    {fetchSpendings()},
-  );
-
+  useEffect(() => {
+    fetchSpendings();
+  }, []); // Empty dependency array to run only once on mount
 
   const spendingData = processData(categorySpendings);
 
-  return <Box className={"statistics-page-container"}>
-
-  <Box className={"statistics-title-text"}>Statistics</Box>
-
-  <Divider />
-
-    <Box className={"statistics-wrapper"}>
-      <Box className="statistics-graph-container">
-        <Box className={"statistics-title-text"}>Total spending:{
-          categorySpendings.reduce((acc, item) => acc += (item.totalSpent), 0)
-        }</Box>
-        <Pie data={spendingData} />
+  return (
+    <Box className={"statistics-page-container"}>
+      <Box className={"statistics-title-text"}>Statistics</Box>
+      <Divider />
+      <Box className={"statistics-wrapper"}>
+        <Box className="statistics-graph-container">
+          <Box className={"statistics-title-text"}>
+            Total spending:
+            {categorySpendings.reduce((acc, item) => acc + item.totalSpent, 0)}
+          </Box>
+          <Pie data={spendingData} />
+        </Box>
       </Box>
     </Box>
-  </Box>;
+  );
 };
